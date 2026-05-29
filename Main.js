0d738c4a653ca41ba75f8e68ce9c012391446b93
PabloRamosRamos <30904927+PabloRamosRamos@users.noreply.github.com>
2026-05-29 10:03:27 -0400

    bug Scripts_Organizaciones

--- Main.js
+++ Main.js
@@ -6,8 +6,8 @@ const GO_PES_V2 = {
   PROGRAM_TITLE: 'Gestor Operativo PES',
   SUBTITLE: 'Programa Estamos Seguros Â· Municipalidad de Providencia',
   VERSION: '2.1.3',
-  BUILD: '66f13f2',
-  BUILD_DATE: '20260528',
+  BUILD: '32de1de',
+  BUILD_DATE: '20260529',
   ENVIRONMENT: 'DEV',
   SUPERUSERS: [
     'pablo.ramos@providencia.cl',

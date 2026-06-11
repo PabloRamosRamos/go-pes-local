524f6eb0c2aaa6b5fb2352d41b57b3556977f2fb
PabloRamosRamos <30904927+PabloRamosRamos@users.noreply.github.com>
2026-06-11 15:02:58 -0400

    fecha hitos

--- go-pes-local/Main.js
+++ go-pes-local/Main.js
@@ -6,7 +6,7 @@ const GO_PES_V2 = {
   PROGRAM_TITLE: 'Gestor Operativo PES',
   SUBTITLE: 'Programa Estamos Seguros · Municipalidad de Providencia',
   VERSION: '2.1.900',
-  BUILD: 'd9c8200',
+  BUILD: 'a34da32',
   BUILD_DATE: '20260611',
   ENVIRONMENT: 'DEV',
   SUPERUSERS: [
